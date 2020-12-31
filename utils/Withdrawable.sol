d05df816c5c060b98a09b7db7d4d5b1d9e0f1d99
Jackson Savitraz <jacksonsavitraz@gmail.com>
2020-12-31 15:18:18 -0300

    Imports

--- utils/Withdrawable.sol
+++ utils/Withdrawable.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.6.6;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/tree/master/contracts/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/tree/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/tree/master/contracts/token/ERC20/SafeERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/tree/master/contracts/access/Ownable.sol";
 
